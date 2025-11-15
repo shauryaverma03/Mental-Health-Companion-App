02a6187055a71bfc18360d730f9dcec11730f792
Sachin Yadav <sachinraosahab7@gmail.com>
2025-11-15 14:49:05 +0530

    done

--- backend/server/chat/chat.utils.js
+++
@@ -1,7 +0,0 @@
-function formatChatHistoryForBot(prevMessages) {
-    return prevMessages
-        .map(chat => `${chat.sender}: ${chat.message}`) // Format as 'user: message' or 'bot: message'
-        .join('\n'); // Join all messages by a newline to create a readable conversation history
-}
-
-module.exports = formatChatHistoryForBot
